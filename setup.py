5278d354c5af592d9e4d3690a43a4f9223446562
kiran.narayan <kiran.narayan71@gmail.com>
2023-03-31 14:43:58 +0530

    completed code structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="census-income",
+    version="0.0.1",
+    author="kiran narayan",
+    author_email="kiran.naryan71@gmail.com",
+    packages=find_packages(),
+    install_requires = ["pandas", "numpy"]
+
+    )
